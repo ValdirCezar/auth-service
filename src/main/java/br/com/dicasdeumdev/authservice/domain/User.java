0ffc341225f1ab1349f3aa85db8a60ec5446c497
Valdir Cezar <valdircezar312@gmail.com>
2021-12-10 22:13:45 -0300

    adjusts

--- src/main/java/br/com/dicasdeumdev/authservice/domain/User.java
+++ src/main/java/br/com/dicasdeumdev/authservice/domain/User.java
@@ -3,9 +3,14 @@ package br.com.dicasdeumdev.authservice.domain;
 import br.com.dicasdeumdev.authservice.enumerations.ProfileEnum;
 import lombok.Getter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Getter
+@Entity
 public class User {
 
+    @Id
     private Long id;
     private String name;
     private String cpfOrCnpj;
